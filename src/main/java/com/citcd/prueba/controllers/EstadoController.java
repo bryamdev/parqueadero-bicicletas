0d84f943243d34a942406858758df036ee3e4875
BryamDev <bryamdev.contact@gmail.com>
2021-01-28 17:45:06 -0500

    Controller de estados terminado

--- src/main/java/com/citcd/prueba/controllers/EstadoController.java
+++ src/main/java/com/citcd/prueba/controllers/EstadoController.java
@@ -1,5 +1,7 @@
 package com.citcd.prueba.controllers;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.citcd.prueba.models.dto.Response;
 import com.citcd.prueba.models.entity.Estado;
 import com.citcd.prueba.models.service.IEstadoService;
 
@@ -28,21 +31,32 @@ public class EstadoController {
 		
 		log.info("Si entró al método!");
 		
-		return ResponseEntity.status(HttpStatus.OK).body(estadoService.findAll());
+		Response response = new Response();
+		List<Estado> estados = estadoService.findAll();
+		
+		response.setIsOk(true);
+		response.setResults(estados);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 		
 	}
 	
-	@GetMapping("/{id}")
+	@GetMapping("/porId/{id}")
 	public ResponseEntity<?> obtenerPorId(@PathVariable Long id){
 		
+		Response response = new Response();
 		Estado estado = estadoService.findByID(id);
 		
 		if(estado == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontro Estado con id " + id);
+			response.setIsOk(false);
+			response.setError("No existe un estado con id " + id);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
 		}
 		
+		response.setIsOk(true);
+		response.setResult(estado);
 		
-		return ResponseEntity.status(HttpStatus.OK).body(estado);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 	
 
